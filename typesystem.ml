05f4cd5e6d8bc24a0a9b192a52adbddb6ade06bb
Daniel R. Grayson <dan@math.uiuc.edu>
2012-10-11 14:39:18 -0700

    define TS0-term as "expr"

--- typesystem.ml
+++ typesystem.ml
@@ -5,16 +5,42 @@
 
    *)
 
-type var =				(* variable *)
-  | Ovar of string			(* object *)
-  | Tvar of string			(* type *)
-  | Uvar of string			(* universe *)
+type oVar = string					(* object variable *)
+type tVar = string					(* type variable *)
+type uVar = string					(* universe variable *)
 
-type uLevel =				(* u-level expression *)
+type var =							 (* variable *)
+  | Ovar of oVar
+  | Tvar of tVar
+  | Uvar of uVar
+
+type uLevel =					       (* u-level expression *)
   | Uint of int
   | Uplus of uLevel * int
   | Umax of uLevel * uLevel
 
-type uContext =				(* universe context *)
-    var (* Uvar _ *) list * (uLevel * uLevel) (* equation *) list
+type uContext =						 (* universe context *)
+    uVar list * (uLevel * uLevel) (* equation *) list
 
+type expr =
+  | Uexpr of uLevel
+  | Texpr of tExpr						   (* type *)
+  | Oexpr of oExpr						   (* object *)
+and tExpr =
+  | Tvariable of tVar
+  | UU of uLevel						      (* U *)
+  | El of oExpr
+  | Product of oVar * tExpr * tExpr
+and oExpr =
+  | Ovariable of oVar
+  | Uu of uLevel						   (* u *)
+  | Jj of uLevel * uLevel					   (* j *)
+  | Ev of oVar * oExpr * oExpr * tExpr				   (* ev *)
+  | Lambda of oVar * tExpr * oExpr				   (* lambda *)
+  | Forall of uLevel * uLevel *	oExpr * oExpr			   (* forall *)
+
+(*
+ Local Variables:
+   comment-column: 70
+ End:
+ *)
