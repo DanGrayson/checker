f30266d797c8328142fa8f8487465b1f9a52dc35
Daniel R. Grayson <dan@math.uiuc.edu>
2012-11-11 09:31:10 -0500

    working on substitution

--- test.ts
+++ test.ts
@@ -8,4 +8,4 @@ Print [Pi;x](T1,[U](max((uu1+1+0)+4,uu2))).
 Print [lambda;x](T,e).
 Print [lambda;x]([El](r),[ev;x](f,o,T)).
 Print [forall;x](uu1,uu2,o,o').
-Subst [El](x) [ y / x ].
+Subst [lambda;x](T,[lambda;x](T,[lambda;x](T,v)))[x/v].
