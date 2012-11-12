732f5160189e2f5da46d9f9546fa43465bac25fe
Daniel R. Grayson <dan@math.uiuc.edu>
2012-11-12 10:18:07 -0500

    eliminated dependence of variable type on case of initial letter(s)

--- Makefile
+++ Makefile
@@ -2,10 +2,10 @@ BFLAGS = -cflags -g,-annot -lflags -g -yaccflag -v
 # BFLAGS += -use-menhir
 # BFLAGS += -verbose 0
 SRCFILES = basic.ml typesystem.ml substitute.ml printer.ml simpletyping.ml scheme.ml grammar.mly tokens.mll toplevel.ml checker.ml 
-all: TAGS run.byte
+all: TAGS run
 checker.byte checker.native: $(SRCFILES); ocamlbuild $(BFLAGS) $@
 clean::; ocamlbuild -clean
 TAGS: $(SRCFILES); ( scripts/etags.ocaml $(SRCFILES) && etags test.ts -o - ) >$@
 clean::; rm -f TAGS
-run run.byte: checker.byte; OCAMLRUNPARAM=b ./$< <test.ts
+run: checker.byte; OCAMLRUNPARAM=b ./$< <test.ts
 run.native: checker.native; OCAMLRUNPARAM=b ./$< <test.ts
--- test.ts
+++ test.ts
@@ -1,10 +1,10 @@
 Print_o x.
 Print_t T.
 Print_t [El](x).
-Print_t [U](uu4).
-Print_o [j](uu1, uu2).
-Print_t *[u](uu4).
-Print_u umax((uu1+1+0)+4,2).
+Print_t [U](u4).
+Print_o [j](u1, u2).
+Print_t *[u](u4).
+Print_u umax((u1+1+0)+4,2).
 Print_t [Sigma;x](T,T').
 Print_t [Coprod](T,T').
 Print_t [Coprod;x,x'](T,T',U,U',o).
@@ -12,7 +12,7 @@ Print_t [Empty]().
 Print_t [IC;x,y,z](A,a,B,D,q).
 Print_t [Id](T,x,y).
 
-Print_o [forall;x](uu1,uu2,o,o').
+Print_o [forall;x](u1,u2,o,o').
 
 Print_t Pi x : T1, [U](14).
 Print_t *x.
@@ -20,9 +20,9 @@ Print_o lambda x : T, e.
 Print_o lambda f:T->U, lambda o:T, lambda x : *r, f o.
 Print_t A->B->C.
 
-Type [u](uu4).
-Type [j](uu,uu').
+Type [u](u4).
+Type [j](u,u').
 Type [ev;x](f,o,T).
 Type [ev;x](f,o,*x).
 Type [lambda;x](T,x).
-Type [forall;x](uu1,uu2,o,o').
+Type [forall;x](u1,u2,o,o').
