9cedbd4cc1751b373a9d1c0a4553442932475bcb
Daniel R. Grayson <dan@math.uiuc.edu>
2012-12-09 13:15:04 -0500

    check in a working version of the demo

--- demo.ts
+++ demo.ts
@@ -19,6 +19,22 @@ Define compose1 (T U V:Type) (g:U⟶V) (f:T⟶U) := [lambda;t](T,[ev;_](g,[ev;_]
        (t ⟼ t_has_type_T ⟼ 
        	  ( ev_hastype U >V g ([ev] f t >U) $a ( ev_hastype T >U f t $a $a )))).
 
+Define A (u : Ulevel; u=u) (t : [U](u)) := [El](t);; (El_type u $assumption $0).
+
+Define B (u : Ulevel) (t : [U](u)) := [El](t);; (El_type u $a $0).
+
+Variable u1 : Ulevel.
+
+Define C := [u](u1) : [U]([next](u1));; (u_univ $a).
+
+Check LF (B ([next] u1) ([u] u1) (u_univ u1)).
+
+Check LF ∏_istype.
+
+Define E (u1 u2 u3:Ulevel)(K:Type) := K⟶K;; (∏_istype $a >$a >>$a ) .
+
+End.
+
 Define compose2 (T U V:Type) (g:U⟶V) (f:T⟶U) := [lambda;t](T,[ev;_](g,[ev;_](f,t,U),V)) : T ⟶ V ;
       # improved TS syntax
       λ_hastype
@@ -28,20 +44,6 @@ Define compose2 (T U V:Type) (g:U⟶V) (f:T⟶U) := [lambda;t](T,[ev;_](g,[ev;_]
        $a 
        (t  ⟾  h  ⟾  
         ev_hastype U (_ ⟾ V) g (f t) $a ( ev_hastype T (_ ⟾ U) f t $a $a )).
-
-Define A (u : Ulevel; u=u) (t : [U](u)) := [El](t); (El_type u $assumption $0).
-
-Define B (u : Ulevel) (t : [U](u)) := [El](t); (El_type u $a $0).
-
-Variable u1 : Ulevel.
-
-Define C := [u](u1) : [U]([next](u1)); (u_univ $a).
-
-Check LF (B ([next] u1) ([u] u1) (u_univ u1)).
-
-Check LF ∏_istype.
-
-Define E (u1 u2 u3:Ulevel)(K:Type) := K⟶K; (∏_istype $a >$a >>$a ) .
 
 #   Local Variables:
 #   compile-command: "make demo "
