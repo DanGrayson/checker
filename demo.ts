464d0e88c49942c6eae46f5a6a01f5e9e5718a8c
Daniel R. Grayson <dan@math.uiuc.edu>
2012-12-09 20:17:45 -0500

    put the demo file into shape; the new LF-in-TS syntax for derivations is 76% than the corresponding LF syntax

--- demo.ts
+++ demo.ts
@@ -1,23 +1,23 @@
 # -*- coding: utf-8 -*-
 
-Define compose (T U V:Type) (g:U⟶V) (f:T⟶U) (t:T) := [ev;_](g,[ev;_](f,t,U),V) : V ;; (
-       ev_hastype U (_ ⟼ V) g ([ev] f t (_ ⟼ U)) $a ( 
-       ev_hastype T (_ ⟼ U) f t $a $a 
-       ))  .
+# the next 4 examples show how the new TS syntax for derivations is 24% smaller than the corresponding LF syntax
+# it's designed to replace
 
-Define compose2 (T U V:Type) (g:U⟶V) (f:T⟶U) := [lambda;t](T,[ev;_](g,[ev;_](f,t,U),V)) : T ⟶ V ;
-      [λ_hastype] (T, _ ⟾ V, t ⟾ g (f t), $a, t ⟾ h ⟾ 
-        [ev_hastype]( U, _ ⟾ V, g, f t, $a, [ev_hastype](T, _ ⟾ U, f, t, $a, $a ))).
+Define compose1 (T U V:Type) (g:U⟶V) (f:T⟶U) (t:T) := [ev;_](g,[ev;_](f,t,U),V) : V ;; (
+       # LF syntax
+       ev_hastype U (_ ⟼ V) g ([ev] f t (_ ⟼ U)) $a (ev_hastype T (_ ⟼ U) f t $a $a)).
 
-Define compose1 (T U V:Type) (g:U⟶V) (f:T⟶U) := [lambda;t](T,[ev;_](g,[ev;_](f,t,U),V)) : T ⟶ V ;;
+Define compose1' (T U V:Type) (g:U⟶V) (f:T⟶U) (t:T) := [ev;_](g,[ev;_](f,t,U),V) : V ;
+       # new TS syntax
+       [ev_hastype](U, _ ⟾ V, g, f t, $a, [ev_hastype](T, _ ⟾ U, f, t, $a, $a)).
+
+Define compose2 (T U V:Type) (g:U⟶V) (f:T⟶U) := [lambda;t](T,[ev;_](g,[ev;_](f,t,U),V)) : T ⟶ V ;;
       # LF syntax
-      (λ_hastype
-       T
-       >V
-       (t ⟼ ([ev] g ([ev] f t >U) >V) ) 
-       $a 
-       (t ⟼ t_has_type_T ⟼ 
-       	  ( ev_hastype U >V g ([ev] f t >U) $a ( ev_hastype T >U f t $a $a )))).
+      (λ_hastype T (_ ⟼ V) (t ⟼ ([ev] g ([ev] f t (_ ⟼ U)) (_ ⟼ V)) ) $a (t ⟼ _ ⟼ (ev_hastype U (_ ⟼ V) g ([ev] f t (_ ⟼ U)) $a (ev_hastype T (_ ⟼ U) f t $a $a )))).
+
+Define compose2' (T U V:Type) (g:U⟶V) (f:T⟶U) := [lambda;t](T,[ev;_](g,[ev;_](f,t,U),V)) : T ⟶ V ;
+      # new TS syntax
+      [λ_hastype](T, _ ⟾ V, t ⟾ g (f t), $a, t ⟾ _ ⟾ [ev_hastype](U, _ ⟾ V, g, f t, $a, [ev_hastype](T, _ ⟾ U, f, t, $a, $a))).
 
 Define A (u : Ulevel; u=u) (t : [U](u)) := [El](t);; (El_type u $assumption $0).
 
@@ -31,7 +31,7 @@ Check LF (B ([next] u1) ([u] u1) (u_univ u1)).
 
 Check LF ∏_istype.
 
-Define E (u1 u2 u3:Ulevel)(K:Type) := K⟶K;; (∏_istype $a >$a >>$a ) .
+Define E (u1 u2 u3:Ulevel)(K:Type) := K⟶K;; (∏_istype $a (_ ⟼ $a) (_ ⟼ _ ⟼ $a) ).
 
 End.
 
