b51897b2ca82a2ddcd57f2d45148f9210b18323f
Daniel R. Grayson <dan@math.uiuc.edu>
2013-01-09 19:59:21 -0600

    finish translating Dan Licata's ts.elf into test/ts-elf.ts

--- test/ts-elf.ts
+++ test/ts-elf.ts
@@ -1,26 +1,48 @@
 # Here we translate the file ../NOTES/ts.elf into our syntax.
 
 Axiom LF istype_Empty : istype @[Empty].
-Axiom LF istype_Forall : (T1:texp) -> (T2:oexp->texp) -> ((x:oexp) -> hastype x T1 -> istype (T2 x)) -> istype (@[Pi] T1 T2).
-Axiom LF hastype_lambda : (T1:texp) -> (T2:oexp->texp) -> (O:oexp->oexp) -> ((x:oexp) -> hastype x T1 -> hastype (O x) (T2 x)) -> hastype (@[lambda] T1 O) (@[Pi] T1 T2).
-Axiom LF hastype_ev : (T1:texp) -> (T2:oexp->texp) -> (F:oexp) -> (O:oexp) -> hastype O T1 -> hastype F (@[Pi] T1 T2) -> hastype (@[ev] F O T2) (T2 O).
-Axiom LF hastype_empty_r : (O:oexp) -> (T:texp) -> hastype O @[Empty] -> istype T -> hastype (@[empty_r] T O) T.
-Axiom LF hastype_eq : (T1:texp) -> (T2:texp) -> (O:oexp) -> tequal T1 T2 -> hastype O T2 -> hastype O T1.
+
+Axiom LF istype_Forall : (T1:texp) -> (T2:oexp->texp) -> 
+      ((x:oexp) -> hastype x T1 -> istype (T2 x)) -> istype (@[Pi] T1 T2).
+
+Axiom LF hastype_lambda : (T1:texp) -> (T2:oexp->texp) -> (O:oexp->oexp) -> 
+      ((x:oexp) -> hastype x T1 -> hastype (O x) (T2 x)) -> 
+      hastype (@[lambda] T1 O) (@[Pi] T1 T2).
+
+Axiom LF hastype_ev : (T1:texp) -> (T2:oexp->texp) -> (F:oexp) -> (O:oexp) -> 
+      hastype O T1 -> hastype F (@[Pi] T1 T2) -> hastype (@[ev] F O T2) (T2 O).
+
+Axiom LF hastype_empty_r : (O:oexp) -> (T:texp) -> hastype O @[Empty] -> 
+      istype T -> hastype (@[empty_r] T O) T.
+
+Axiom LF hastype_eq : (T1:texp) -> (T2:texp) -> (O:oexp) -> tequal T1 T2 -> 
+      hastype O T2 -> hastype O T1.
+
 Axiom LF o_eq_beta : (T1:texp) -> (T2:oexp->texp) -> (O1:oexp) -> (O2:oexp->oexp) ->
-      		((x:oexp) -> hastype x T1 -> hastype (O2 x) (T2 x)) ->
-		hastype O1 T1 ->
-		oequal (@[ev] (@[lambda] T1 O2) O1 T2) (O2 O1) (T2 O1).
-Axiom LF o_eq_app : (T1:texp) -> (T2:oexp->texp) -> (O:oexp) -> (O':oexp) -> (F:oexp) -> (F':oexp) -> 
-      		oequal O O' T1 -> oequal F F' (@[Pi] T1 T2) ->
-		oequal (@[ev] F O T2) (@[ev] F' O' T2) (T2 O).
+      (x:oexp) -> hastype x T1 -> hastype (O2 x) (T2 x) ->
+      hastype O1 T1 ->
+      oequal (@[ev] (@[lambda] T1 O2) O1 T2) (O2 O1) (T2 O1).
+
+Axiom LF o_eq_app : (T1:texp) -> (T2:oexp->texp) -> (O:oexp) -> (O':oexp) -> 
+      (F:oexp) -> (F':oexp) -> 
+      oequal O O' T1 -> oequal F F' (@[Pi] T1 T2) ->
+      oequal (@[ev] F O T2) (@[ev] F' O' T2) (T2 O).
+
 Axiom LF o_eq_empty_eta : (O:oexp) -> (O1:oexp) -> (O2:oexp) -> (A:texp) -> 
-      		hastype O @[Empty] -> hastype O1 A -> hastype O2 A -> oequal O1 O2 A.
+      hastype O @[Empty] -> hastype O1 A -> hastype O2 A -> oequal O1 O2 A.
+
 Axiom LF t_eq_empty_eta : (O:oexp) -> (A:texp) -> (B:texp) -> 
-            	istype A -> istype B -> hastype O @[Empty] -> tequal A B.
+      hastype O @[Empty] -> istype A -> istype B -> tequal A B.
 
-End.
-
-Theorem LF foo (T1:texp) -> (T2:texp) -> (T3:texp) -> (F:oexp) -> (O:oexp)
+Theorem LF foo : (T1:texp) -> (T2:texp) -> (T3:texp) -> (F:oexp) -> (O:oexp) -> (Bad:oexp) ->
+      istype T1 -> istype T2 -> hastype O T1 -> hastype F (@[Pi] T2 (_ |-> T3)) ->
+      hastype Bad @[Empty] -> hastype (@[ev] F O (_ |-> T3)) T3 
+      :=
+      T1 |-> T2 |-> T3 |-> F |-> O |-> Bad |-> dT1 |-> dT2 |-> dO |-> dF |-> dBad |-> 
+      (hastype_ev T2 (_ |-> T3) F O 
+          (hastype_eq T2 T1 O 
+	  	(t_eq_empty_eta Bad T2 T1 dBad dT2 dT1) dO)
+	  dF).
 
 Show 20.
 
