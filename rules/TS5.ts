ac6819b13e90256211e93385df4709ef5902358b
Daniel R. Grayson <dan@math.uiuc.edu>
2013-02-02 11:24:52 -0500

    working on eta for nat

--- rules/TS5.ts
+++ rules/TS5.ts
@@ -2,11 +2,11 @@
 
 Include "rules/TS4.ts".
 
-Axiom 15.5.1 nat_hastype ⊢ nat : UU[uu0].
+Axiom 15.5.1 nat_hastype [ nat : UU[uu0] ].
 
-Lemma Nat_istype [ Nat Type ] := El_istype[uu0,nat,CDR,nat_hastype[CDR]].
+Lemma Nat_istype [ Nat Type ] := El_istype[uu0,nat,CDR,_].
 
-Axiom 15.5.2 O_hastype ⊢ O : Nat.
+Axiom 15.5.2 O_hastype [ O : Nat ].
 
 Axiom 15.5.3 S_hastype [ S : Nat -> Nat ].
 
@@ -27,8 +27,37 @@ Lemma nat_S_reduction_sanity1 { x : Nat ⊢ T Type } { ⊢ o1 : T[O] } { ⊢ o2 
       T ⟾ T' ⟾ o1 ⟾ o1' ⟾ o2 ⟾ o2' ⟾ n ⟾ n' ⟾ 
       nat_r_hastype[
       	T, o1, o2, @[ev;_][S,n,Nat,Nat], CDR,
-	T', o1', o2', ev_hastype[Nat,_⟾Nat,S,n,CDR,Nat_istype,_⟾_⟾Nat_istype,S_hastype,_]
+	T',_, _, ev_hastype[Nat,_⟾Nat,S,n,CDR,_,_,_,_]
 	].
+
+Lemma nat_S_reduction_sanity2 { x : Nat ⊢ T Type } { ⊢ o1 : T[O] } { ⊢ o2 : ∏ x:Nat, T[x] -> T[ @[ev;_][S,x,Nat,Nat] ] } 
+
+      { ⊢ n : Nat } 
+
+      [   @[ev;_][@[ev;x][o2,n,Nat,T[x]->T[@[ev;_][S,x,Nat,Nat]]],@[nat_r][o1,o2,n,T],T[n],T[@[ev;_][S,n,Nat,Nat]]]
+	  :
+	  T[@[ev;_][S,n,Nat,Nat]] ] :=
+
+      T ⟾ T' ⟾ o1 ⟾ o1' ⟾ o2 ⟾ o2' ⟾ n ⟾ n' ⟾ 
+      ev_hastype[
+          T[n],
+	  _⟾T[@[ev;_][S,n,Nat,Nat]],
+	  @[ev;x][o2,n,Nat,T[x]->T[@[ev;_][S,x,Nat,Nat]]],
+	  @[nat_r][o1,o2,n,T],
+	  CDR,
+          T'[n,_],
+	  _⟾_⟾T'[@[ev;_][S,n,Nat,Nat],ev_hastype[Nat,_⟾Nat,S,n,CDR,_,_,_,_]],
+	  ev_hastype[
+	  	Nat,
+		x ⟾ T[x]->T[@[ev;_][S,x,Nat,Nat]],
+		o2,
+		n,
+		CDR,
+		_,
+		x⟾x'⟾∏_istype[T[x],_⟾T[@[ev;_][S,x,Nat,Nat]],CDR,T'[x,x'],_⟾_⟾T'[@[ev;_][S,x,Nat,Nat],ev_hastype[Nat,_⟾Nat,S,x,CDR,_,_,_,_]]],
+		o2',
+		_],
+	  nat_r_hastype[T,o1,o2,n,CDR,T',_,_,_]].
 
 Axiom 15.3.1.3 nat_S_reduction { x : Nat ⊢ T Type } { ⊢ o1 : T[O] } { ⊢ o2 : ∏ x:Nat, T[x] -> T[ @[ev;_][S,x,Nat,Nat] ] } 
 
@@ -36,9 +65,29 @@ Axiom 15.3.1.3 nat_S_reduction { x : Nat ⊢ T Type } { ⊢ o1 : T[O] } { ⊢ o2
 
       [   @[nat_r][o1,o2,@[ev;_][S,n,Nat,Nat],T] 
           ≡ 
-  	  @[ev;_][@[ev;x][o2,n,Nat,T[x]->T[@[ev;_][S,x,Nat,Nat]]],@[nat_r][o1,o2,n,T],Nat,T[@[ev;_][S,n,Nat,Nat]]]
+	  @[ev;_][@[ev;x][o2,n,Nat,T[x]->T[@[ev;_][S,x,Nat,Nat]]],@[nat_r][o1,o2,n,T],T[n],T[@[ev;_][S,n,Nat,Nat]]]
 	  :
 	  T[@[ev;_][S,n,Nat,Nat]] ].
+
+# Lemma nat_eta_sanity { x : Nat ⊢ T Type }
+
+#       { ⊢ o1 : T[O] } 
+
+#       { ⊢ o2 : ∏ x:Nat, T[x] -> T[ @[ev;_][S,x,Nat,Nat] ] }
+
+#       { ⊢ n : Nat } { ⊢ t : T[n] } [ 
+
+#       		@[ev;_][@[ev;x][o2,n,Nat,T[x]->T[@[ev;_][S,x,Nat,Nat]]],t,T[n],T[@[ev;_][S,n,Nat,Nat]]]
+
+# 		: 
+
+# 		T[@[ev;_][S,n,Nat,Nat]]
+
+# 	   ] :=
+
+#    T ⟾ T' ⟾ o1 ⟾ o1' ⟾ o2 ⟾ o2' ⟾ n ⟾ n' ⟾ t ⟾ t' ⟾ 
+
+#    _ .
 
 Axiom nat_eta { x : Nat ⊢ T Type }
 
