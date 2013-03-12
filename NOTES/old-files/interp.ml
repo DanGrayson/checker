f538fe6a6ae64962e09ec66f210c6be205c18d44
Daniel R. Grayson <dan@math.uiuc.edu>
2013-03-12 13:30:48 -0400

    clean up scheme inner interpreter

--- NOTES/old-files/interp.ml
+++ NOTES/old-files/interp.ml
@@ -10,102 +10,97 @@
 
 *)
 
-exception NotImplemented
-
 type variable = string
 
-type expr =
-  | BooleanS of bool
-  | IntegerS of int
-  | SymbolS of string
-  | Function of variable list * expr
+type computation =
+  | Value of value
+  | Function of variable list * computation
   | Variable of variable
-  | If of expr * expr * expr
-  | Cons of expr * expr
-  | Car of expr
-  | Cdr of expr
-  | SetCar of expr * expr
-  | SetCdr of expr * expr
-  | Apply of expr * expr list
-  | Set of variable * expr
-  | LetCC of variable * expr
-  | Throw of expr * expr
-  | NilS
-
-type bindings = (variable * value ref) list
+  | If of computation * computation * computation
+  | Cons of computation * computation
+  | Car of computation
+  | Cdr of computation
+  | SetCar of computation * computation
+  | SetCdr of computation * computation
+  | Apply of computation * computation list
+  | Set of variable * computation
+  | LetCC of variable * computation
+  | Throw of computation * computation
+and bindings = (variable * value ref) list
 and value =
   | NilV
   | SymbolV of string
   | BooleanV of bool
   | IntegerV of int
   | ConsCell of value ref * value ref
-  | FuncClosure of bindings * variable list * expr
-  | ContClosure of bindings * continuations
-and continuations = continuation list
-and continuation =
-  | IfCont of expr * expr
-  | ConsCont1 of expr | ConsCont2 of value
-  | SetCarCont1 of expr | SetCarCont2 of value
-  | SetCdrCont1 of expr | SetCdrCont2 of value
-  | ThrowCont1 of expr | ThrowCont2 of value
-  | CarCont
-  | CdrCont
-  | ApplyCont1 of expr list
-  | ApplyCont2 of value * (value ref) list * expr list
-  | SetCont of variable
+  | FuncClosure of bindings * variable list * computation
+  | ContClosure of bindings * continuation
+and continuation = command list
+and command =
+  | IfCmd of computation * computation
+  | ConsCmd1 of computation | ConsCmd2 of value
+  | SetCarCmd1 of computation | SetCarCmd2 of value
+  | SetCdrCmd1 of computation | SetCdrCmd2 of value
+  | ThrowCmd1 of computation | ThrowCmd2 of value
+  | CarCmd
+  | CdrCmd
+  | ApplyCmd1 of computation list
+  | ApplyCmd2 of value * (value ref) list * computation list
+  | SetCmd of variable
 
 let rec eval e r k =
   match e with
-  | NilS ->           continue NilV r k
-  | BooleanS b ->     continue (BooleanV b) r k
-  | IntegerS b ->     continue (IntegerV b) r k
-  | SymbolS s ->      continue (SymbolV s) r k
+  | Value v ->        continue v r k
   | Function(x, e) -> continue (FuncClosure(r, x, e)) r k
   | Variable x ->     continue !(try List.assoc x r with Not_found -> (Printf.printf "unset variable: %s\n" x; raise Not_found)) r k
-  | If(e, e1, e2) ->  eval e r (IfCont(e1, e2) :: k)
-  | Cons(e1, e2) ->   eval e1 r (ConsCont1 e2 :: k)
-  | Car e ->          eval e r (CarCont :: k)
-  | Cdr e ->          eval e r (CdrCont :: k)
-  | SetCar(e1, e2) -> eval e1 r (SetCarCont1 e2 :: k)
-  | SetCdr(e1, e2) -> eval e1 r (SetCdrCont1 e2 :: k)
-  | Apply(f, args) -> eval f r (ApplyCont1 args :: k)
-  | Set(x, e) ->      eval e r (SetCont x :: k)
+  | If(e, e1, e2) ->  eval e r (IfCmd(e1, e2) :: k)
+  | Cons(e1, e2) ->   eval e1 r (ConsCmd1 e2 :: k)
+  | Car e ->          eval e r (CarCmd :: k)
+  | Cdr e ->          eval e r (CdrCmd :: k)
+  | SetCar(e1, e2) -> eval e1 r (SetCarCmd1 e2 :: k)
+  | SetCdr(e1, e2) -> eval e1 r (SetCdrCmd1 e2 :: k)
+  | Apply(f, args) -> eval f r (ApplyCmd1 args :: k)
+  | Set(x, e) ->      eval e r (SetCmd x :: k)
   | LetCC(x, e) ->    eval e ((x, ref (ContClosure(r, k))) :: r) k
-  | Throw(e1, e2) ->  eval e1 r (ThrowCont1 e2 :: k)
+  | Throw(e1, e2) ->  eval e1 r (ThrowCmd1 e2 :: k)
 and continue v r k =
   match v, k with
-  | v, [] ->                                          v
+  | v, [] ->                                            v
 
-  | v, (ConsCont1 e :: k) ->                             eval e r (ConsCont2 v :: k)
-  | v, (ConsCont2 car :: k) ->                           continue (ConsCell(ref car, ref v)) r k
+  | car, ConsCmd1 cdr :: k ->                           eval cdr r (ConsCmd2 car :: k)
+  | cdr, ConsCmd2 car :: k ->                           continue (ConsCell(ref car, ref cdr)) r k
 
-  | v, (SetCarCont1 e :: k) ->                           eval e r (SetCarCont2 v :: k)
-  | v, (SetCarCont2 ConsCell(r1, r2) :: k) ->            (r1 := v; continue NilV r k)
-  | v, (SetCarCont2 _ :: k) ->                           failwith "setcar expected a cons cell"
+  | v, SetCarCmd1 e :: k ->                             eval e r (SetCarCmd2 v :: k)
+  | v, SetCarCmd2 ConsCell(r1, r2) :: k ->              r1 := v; continue NilV r k
+  | v, SetCarCmd2 _ :: k ->                             failwith "setcar expected a cons cell"
 
-  | v, (SetCdrCont1 e :: k) ->                           eval e r (SetCdrCont2 v :: k)
-  | v, (SetCdrCont2 ConsCell(r1, r2) :: k) ->            (r2 := v; continue NilV r k)
-  | v, (SetCdrCont2 _ :: k) ->                           failwith "setcdr expected a cons cell"
+  | v, SetCdrCmd1 e :: k ->                             eval e r (SetCdrCmd2 v :: k)
+  | v, SetCdrCmd2 ConsCell(r1, r2) :: k ->              r2 := v; continue NilV r k
+  | v, SetCdrCmd2 _ :: k ->                             failwith "setcdr expected a cons cell"
 
-  | v, (ThrowCont1 e :: k) ->                            eval e r (ThrowCont2 v :: k)
-  | ContClosure(r, k), (ThrowCont2 v :: _) ->            continue v r k
+  | v, ThrowCmd1 e :: k ->                              eval e r (ThrowCmd2 v :: k)
+  | ContClosure(r, k), ThrowCmd2 v :: _ ->              continue v r k
+  | _, ThrowCmd2 v :: _ ->                              failwith "throw: expected a continuation"
 
-  | (ConsCell(car, cdr)), (CarCont :: k) ->              continue !car r k
-  | _, (CarCont :: k) ->                                 failwith "car: expected a cons cell"
+  | ConsCell(car, cdr), CarCmd :: k ->                  continue !car r k
+  | _, CarCmd :: k ->                                   failwith "car: expected a cons cell"
 
-  | (ConsCell(car, cdr)), (CdrCont :: k) ->              continue !cdr r k
-  | _, (CdrCont :: k) ->                                 failwith "cdr: expected a cons cell"
+  | ConsCell(car, cdr), CdrCmd :: k ->                  continue !cdr r k
+  | _, CdrCmd :: k ->                                   failwith "cdr: expected a cons cell"
 
-  | f, (ApplyCont1(arg :: rest) :: k) ->                 eval arg r (ApplyCont2(f, [], rest) :: k)
-  | FuncClosure(r, [], e), (ApplyCont1 [] :: k) ->       eval e r k
-  | _, (ApplyCont1 [] :: k) ->                           failwith "apply: expected a function"
+  | f, ApplyCmd1(arg :: rest) :: k ->                   eval arg r (ApplyCmd2(f, [], rest) :: k)
+  | FuncClosure(r, [], e), ApplyCmd1 [] :: k ->         eval e r k
+  | _, ApplyCmd1 [] :: k ->                             failwith "apply: expected a function"
 
-  | v, (ApplyCont2(f, vs, e :: es) :: k) ->              eval e r (ApplyCont2(f, (ref v) :: vs, es) :: k)
-  | FuncClosure(r, xs, e), (ApplyCont2(f, vs, []) :: k) -> eval e ((List.combine xs (List.rev vs)) @ r) k
-  | _, (ApplyCont2(f, vs, []) :: k) ->                   failwith "apply: expected a function"
+  | v, ApplyCmd2(f, vs, e :: es) :: k ->                eval e r (ApplyCmd2(f, (ref v) :: vs, es) :: k)
+  | FuncClosure(r, xs, e), ApplyCmd2(f, vs, []) :: k -> eval e ((List.combine xs (List.rev vs)) @ r) k
+  | _, ApplyCmd2(f, vs, []) :: k ->                     failwith "apply: expected a function"
 
-  | (BooleanV b), (IfCont(e1, e2) :: k) ->               eval (if b then e1 else e2) r k
+  | BooleanV b, IfCmd(e1, e2) :: k ->                   eval (if b then e1 else e2) r k
+  | _, IfCmd(e1, e2) :: k ->                            failwith "if: expected a Boolean value"
 
-  | v, (SetCont(x) :: k) ->                              ((List.assoc x r) := v; continue NilV r k)
-
-  | _ ->                                                 failwith "internal error"
+  | v, SetCmd(x) :: k ->                                let x = 
+							  try List.assoc x r
+							  with Not_found -> failwith ("set: undefined variable: " ^ x) in
+							x := v;
+                                                        continue NilV r k
--- NOTES/old-files/schemeGrammar.mly
+++ NOTES/old-files/schemeGrammar.mly
@@ -2,26 +2,19 @@
 open Interp
 %}
 %start expr
-%type <Interp.expr> expr
-%token Tlparen Trparen Tdot Tapost Lambda
+%type <Interp.computation> expr
+%token Tlparen Trparen Tdot Tapost 
 %token <string> Identifier
 %token <int> Integer
 %%
 expr :
    | Identifier { Variable $1 }
-   | Integer { IntegerS $1 }
+   | Integer { Value (IntegerV $1) }
    | Tapost expr { Cons(Variable "quote",$2) }
    | plist { $1 }
 plist :
-   | Tlparen list Trparen { $2 }
-   | Tlparen Trparen { NilS }
-list :
-   | { NilS }
+   | Tlparen llist Trparen { $2 }
+llist :
+   | { Value NilV }
    | expr Tdot expr { Cons($1,$3) }
-   | expr list { Cons($1,$2) }
-pnlist :
-   | Tlparen nlist Trparen { $2 }
-   | Tlparen Trparen { NilS }
-nlist :
-   | { NilS }
-   | expr nlist { Cons($1,$2) }
+   | expr llist { Cons($1,$2) }
