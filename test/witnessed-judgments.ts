4b6183052882c5e12497caae5221981e5acc21ae
Daniel R. Grayson <dan@math.uiuc.edu>
2013-04-02 10:13:02 -0400

    update tests

--- test/witnessed-judgments.ts
+++ test/witnessed-judgments.ts
@@ -6,21 +6,15 @@ Variable f : T -> @U;;
 
 Goal [ T -> @U Type ];;
 
-# Goal { |- B Type } [ B -> @U Type ];;
-
-# Goal LF (B:texp) ⟶ witnessed_istype B ⟶ witnessed_istype (@Pi B (_ ⟼ _ ⟼ @U));;
-
-# Goal LF (B:texp) ⟶ witnessed_istype (@Pi B (_ ⟼ _ ⟼ @U));;
-
 Goal [ f : T -> @U ];;
 
 Goal [ x : T ];;
 
 Goal [ @ev[f,x,T,.@U] : @U ];;
 
-Goal [ @ev[@λ[T,y.@ev[f,y,T,.@U]],x,T,.@U] : @U];;
+Goal [ @ev[@λ[T,y.@ev[f,y,T,.@U]],x,T,.@U] : @U ];;
 
-Lemma a [ @ev[@λ[T,y.@ev[f,y,T,.@U]],x,T,.@U] : @U];;
+Lemma a [ @ev[@λ[T,y.@ev[f,y,T,.@U]],x,T,.@U] : @U ];;
 Check a;;
 
 Goal [ @El[@ev[f,x,T,.@U]] Type ];;
@@ -41,15 +35,17 @@ Goal [ ? : * @λ[T,y.f y] x ≡ * f x ];;
 
 Goal [ ? : * (λ y:T,f y) x ≡ * f x ];;
 
-Goal [ ? : * ((y:T) |-> f y) x ≡ * f x ] ;;
+Goal [ ? : * ((y:T) |-> f y) x ≡ * f x ];;
 
-Goal [ ? : * ((y:T) ⟼ f y) x ≡ * f x ] ;;
+Goal [ ? : * ((y:T) ⟼ f y) x ≡ * f x ];;
+
+Lemma a8 [ ? : * (λ y:T,f y) x ≡ * f x ];;
+
+Check a8;;
 
 End;;
 
-Goal [ ? : x : ? ];;					    # here we could deduce the type, too
-
-Lemma a8 : * (λ y:T,f y) x ≡ * f x;;
+Goal [ x : ? ];;					    # here we could deduce the type, too
 
 # Make head reduction work;;
 # This one is the same as the one above, with objects reversed;;
