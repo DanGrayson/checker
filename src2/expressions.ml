ba8719455639f6ec8e0497d5672f3dad5fb26719
Daniel R. Grayson <dan@math.uiuc.edu>
2013-08-03 17:37:54 -0400

    checker2: inference rules

--- src2/expressions.ml
+++ src2/expressions.ml
@@ -6,24 +6,36 @@ type var =
 let vartostring = function
   | Rel i -> string_of_int i ^ "^"	(* raw form *)
 
-type uHead = | U_next | U_max
-type tHead = | T_El | T_U | T_Pi | T_Sigma | T_Pt
-             | T_Coprod | T_Coprod2 | T_Empty | T_IP | T_Id
-type oHead = | O_u | O_j | O_ev | O_lambda | O_forall | O_pair | O_pr1
-	     | O_pr2 | O_total | O_pt | O_pt_r | O_tt | O_coprod | O_ii1 | O_ii2 | O_sum
-	     | O_empty | O_empty_r | O_c | O_ip_r | O_ip | O_paths | O_refl | O_J | O_rr0
-	     | O_rr1 | O_nat | O_nat_r | O_O | O_S
-type wHead = | W_Wrefl | W_Wsymm | W_Wtrans | W_wrefl | W_wsymm | W_wtrans | W_wconv
-	     | W_wconveq | W_weleq | W_wpi1 | W_wpi2 | W_wlam | W_wl1 | W_wl2 | W_wev
-	     | W_wevt1 | W_wevt2 | W_wevf | W_wevo | W_wbeta | W_weta
+type expr_head = 
 
-type expr_head = | U of uHead | T of tHead | O of oHead | W of wHead | V of var
+    Var of var
+
+  | U_next | U_max
+
+  | T_El | T_U | T_Pi | T_Sigma | T_Pt
+  | T_Coprod | T_Coprod2 | T_Empty | T_IP | T_Id
+
+  | O_u | O_j | O_ev | O_lambda | O_forall | O_pair | O_pr1
+  | O_pr2 | O_total | O_pt | O_pt_r | O_tt | O_coprod | O_ii1 | O_ii2 | O_sum
+  | O_empty | O_empty_r | O_c | O_ip_r | O_ip | O_paths | O_refl | O_J | O_rr0
+  | O_rr1 | O_nat | O_nat_r | O_O | O_S
+
+  | W_Wrefl | W_Wsymm | W_Wtrans | W_wrefl | W_wsymm | W_wtrans | W_wconv
+  | W_wconveq | W_weleq | W_wpi1 | W_wpi2 | W_wlam | W_wl1 | W_wl2 | W_wev
+  | W_wevt1 | W_wevt2 | W_wevf | W_wevo | W_wbeta | W_weta
 
 type expr =
   | BASIC of expr_head * expr_list
 and expr_list =
   | ARG of int * expr * expr_list	(* the integer gives the number of variables bound in expr *)
   | END
+
+let ( @ ) h s = BASIC(h,s)
+let ( ** ) x s = ARG(0,x,s)		(* right associative *)
+let ( **. ) x s = ARG(1,x,s)
+let ( **.. ) x s = ARG(2,x,s)
+let ( **... ) x s = ARG(3,x,s)
+let var_to_expr v = (Var v) @ END
 
 type judgment = 
     (* If the last expr is missing, then it represents a hypothesis, if present, a conclusion and definition. *)
@@ -41,8 +53,6 @@ type judgment =
 							      Use j=>k as an abbreviation. *)
 
 (** functions *)
-
-let var_to_expr v = BASIC(V v,END)
 
 let rec rel_shift_expr limit shift e =
   if shift = 0 then e else
@@ -62,7 +72,7 @@ let rec rel_shift_expr limit shift e =
 
 and rel_shift_head limit shift h = 
   match h with
-  | V (Rel i) when i >= limit -> V (Rel (shift+i))
+  | Var (Rel i) when i >= limit -> Var (Rel (shift+i))
   | _ -> h
 
 and rel_shift_jgmt limit shift j =
--- src2/rules.ml
+++ src2/rules.ml
@@ -4,11 +4,14 @@ open Error
 open Expressions
 
 type rule_name =
-    R_Empty | R_Abstr | R_TypeVar | R_Parm
+
+  | R_Empty | R_Abstr | R_TypeVar | R_Parm | R_TypeEquality | R_ObjectEquality | R_TypeRefl | R_ObjectRefl | R_Wrefl
+  | R_Wsymm | R_Wtrans
 
 module InferenceRules :
     sig
       type statement
+      val expose : statement -> judgment list
       val rule : rule_name * statement list -> statement
     end =
   struct
@@ -16,14 +19,35 @@ module InferenceRules :
 				      implications, stored in reverse order, with the final conclusion at the top of the
 				      list.  If we were using contexts, we would write j,i |- h instead; we refrain from
 				      separating the context from the final conclusion. *)
+    let expose s = s
     let rule n = match n with
-    | R_Empty, []			(* empty statement *)
-      -> []
-    | R_Abstr, [q :: p :: s]		(* abstraction *)
-      -> J_Pi(p,q) :: s
-    | R_TypeVar, [s]			(* new type variable or parameter *)
-      -> J_istype(None) :: s
-    | R_Parm, [J_istype(Some t) :: s]	(* new axiom or parameter *)
-      -> J_hastype(t,None) :: s
+    | R_Empty, [] ->			(* empty statement *)
+	[]
+    | R_Abstr, [q :: p :: s] ->		(* abstraction *)
+	J_Pi(p,q) :: s
+    | R_TypeVar, [s] ->			(* assume we are given a type *)
+	J_istype(None) :: s
+    | R_Parm, [J_istype(Some t) :: s] -> (* assume a type has an element *)
+	J_hastype(t,None) :: s
+    | R_TypeEquality, [J_istype(Some t) :: s; J_istype(Some t') :: s'] -> (* assume two types are equal *)
+	if s == s' 
+	then J_type_equality(t,t',None) :: s
+	else raise InternalError
+    | R_ObjectEquality, [J_hastype(t,Some o) :: s; J_hastype(t',Some o') :: s'] -> (* assume two objects are equal *)
+	if s == s' && t == t'
+	then J_object_equality(t,o,o',None) :: s
+	else raise InternalError
+    | R_Wrefl, [J_istype(Some t) :: s; J_istype(Some t') :: s'] -> (* record that two types are equal *)
+	if s == s' && t = t'
+	then J_type_equality(t,t',Some(BASIC(W_Wrefl,END))) :: s
+	else raise InternalError
+    | R_Wsymm, [J_type_equality(t,t',Some p) :: s]
+      -> (* symmetry of type equality *)
+	J_type_equality(t',t,Some(W_Wsymm @ p ** END)) :: s
+    | R_Wtrans , [J_type_equality(t,t',Some p) :: s; J_type_equality(t'',t''',Some p') :: s']
+      -> (* transitivity of type equality *)
+	if s == s' && t' == t''
+	then J_type_equality(t,t''',Some(W_Wtrans @ p ** p' ** END)) :: s
+	else raise InternalError
     | _ -> raise InternalError
   end
