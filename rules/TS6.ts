9b60cd6f3e0b2ecdc46b2cee398d35544a59d8fd
Daniel R. Grayson <dan@math.uiuc.edu>
2013-08-06 20:22:25 -0400

    reorder premisses in J rule

--- rules/TS6.ts
+++ rules/TS6.ts
@@ -14,13 +14,13 @@ Axiom 11.5.3 refl_hastype { ⊢ T Type, o : T } ⊢ @refl[T,o] : Id[T,o,o];;
 
 Axiom 11.5.4 J_hastype
 
-      { ⊢ T Type, a b:T, i:Id[T,a,b] }
+      { ⊢ T Type, a:T }
 
       { x:T, e:Id[T,a,x] ⊢ S Type }
 
       { ⊢ q : S[a,@refl[T,a]] }
 
-        ⊢ @J[T,a,b,q,i,S] : S[b,i];;
+      { ⊢ b:T, i:Id[T,a,b] } ⊢ @J[T,a,b,q,i,S] : S[b,i];;
 
 Axiom 11.3.2 El_paths_reduction
 
